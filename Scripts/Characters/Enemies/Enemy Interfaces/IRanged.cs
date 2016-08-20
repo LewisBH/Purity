9c03443a5f50bb6cf0049585d01c856cd7a02ed3
Lewis Wickham <LewisWickham_BrokenHelix@hotmail.com>
2016-08-20 16:57:16 +0100

    removed the first things

--- Scripts/Characters/Enemies/Enemy Interfaces/IRanged.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface IRanged {
-
-    void RangedAttack();
-}
--- Scripts/NPCDialogue/CharacterSpriteList.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public class CharacterSpriteList : MonoBehaviour {
-
-    public List<Sprite> spriteList;
-}
