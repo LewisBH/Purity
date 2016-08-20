3df47bfc8b4f133382597df2ea365f61d90a5445
BrokenHelix <lwick1994@hotmail.com>
2016-08-20 16:33:11 +0100

    Add files via upload

---
+++ Editor/GridWindow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class GridWindow : EditorWindow {
+
+    Grid grid;
+
+	public void Init()
+    {
+        grid = (Grid)FindObjectOfType(typeof(Grid));
+    }
+
+    void OnGUI()
+    {
+        grid.colour = EditorGUILayout.ColorField(grid.colour, GUILayout.Width(200));
+    }
+}
